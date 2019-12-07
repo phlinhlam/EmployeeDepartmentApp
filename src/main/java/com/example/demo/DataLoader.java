5d999653116b42f02ff5f8cbbed850881a2a05a8
Phuong Linh Lam <PhuongLinhLam@PhuongLinhs-MacBook-Pro.local>
2019-12-07 17:43:37 -0500

    cloudinary completed

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -30,11 +30,15 @@ public class DataLoader implements CommandLineRunner {
 
         User user1 = new User("linh@email.com","password","Admin","Lam",true,"admin");
         user1.setRoles(Arrays.asList(adminRole));
+        user1.setUrl("https://res.cloudinary.com/phlinhlam/image/upload/v1575757235/iuyzonvkbyqhbmq1ctjp.png");
+
         userRepository.save(user1);
 
         User user2 = new User("user@gmail.com","password","User","Last Name",true,"user");
         user2.setRoles(Arrays.asList(userRole));
+        user2.setUrl("https://res.cloudinary.com/phlinhlam/image/upload/v1575757235/iuyzonvkbyqhbmq1ctjp.png");
         userRepository.save(user2);
+
 
         //add a department to database
         Department department1 = new Department();
